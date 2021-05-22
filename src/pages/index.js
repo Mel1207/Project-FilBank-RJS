2068eadc086f3eb0b157869fb25020ed45e218c8
Mel <meljohnpualon@gmail.com>
2021-05-23 01:17:11 +0800

    added sidebar components, sidebar elements, home component and toggle functions from close and bar icon

---
+++ src/pages/index.js
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Sidebar from '../components/sidebar'
+
+const Home = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen)
+    }
+
+    return (
+        <>
+            <Sidebar isOpen={isOpen} toggle={toggle}></Sidebar>
+            <Navbar toggle={toggle}></Navbar>
+        </>
+    )
+}
+
+export default Home
